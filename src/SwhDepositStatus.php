6b9d146863a78578ad243edac92e184c9a275e99
Yannick Schillo <yschillo@dagstuhl.de>
2024-09-17 17:17:33 +0200

    Implement `SwhDepositStatus::getDescription` and `SwhDepositStatus::isFinal`

--- src/SwhDepositStatus.php
+++ src/SwhDepositStatus.php
@@ -11,4 +11,44 @@ enum SwhDepositStatus: string
     case Loading = "loading";
     case Done = "done";
     case Failed = "failed";
+
+    public function getDescription(): string
+    {
+        return match($this) {
+            SwhDepositStatus::Partial =>
+                "multipart deposit is still ongoing",
+            SwhDepositStatus::Deposited =>
+                "deposit completed, ready for checks",
+            SwhDepositStatus::Rejected =>
+                "deposit failed the checks",
+            SwhDepositStatus::Verified =>
+                "content and metadata verified, ready for loading",
+            SwhDepositStatus::Loading =>
+                "loading in-progress",
+            SwhDepositStatus::Done =>
+                "loading completed successfully",
+            SwhDepositStatus::Failed =>
+                "the deposit loading has failed",
+        };
+    }
+
+    public function isFinal(): bool
+    {
+        return match($this) {
+            SwhDepositStatus::Partial =>
+                false,
+            SwhDepositStatus::Deposited =>
+                false,
+            SwhDepositStatus::Rejected =>
+                true,
+            SwhDepositStatus::Verified =>
+                false,
+            SwhDepositStatus::Loading =>
+                false,
+            SwhDepositStatus::Done =>
+                true,
+            SwhDepositStatus::Failed =>
+                true,
+        };
+    }
 }
